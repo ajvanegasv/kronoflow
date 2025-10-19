18bfa12649bbc724a4937659e2fa02bd149073fe
Alvaro Vanegas <alvaro@ajvanegasv.dev>
2025-10-18 20:59:54 -0500

    Change `executionTime` type in `Task` schema from `LocalTime` to `long` for consistency.

--- composeApp/src/jvmMain/kotlin/ajvanegasv/dev/kronoflow/data/datasource/local/schemas/Task.kt
+++ composeApp/src/jvmMain/kotlin/ajvanegasv/dev/kronoflow/data/datasource/local/schemas/Task.kt
@@ -12,7 +12,7 @@ object Task: IntIdTable("Tasks") {
     var name = varchar("name", 255)
     var description = varchar("description", 255).nullable()
     var status = varchar("status", 255)
-    var executionTime = time("executionTime").default(LocalTime(0, 0))
+    var executionTime = long("executionTime").default(0)
     var dueDate = date("dueDate").nullable()
     var space = optReference("space", Space.id, onDelete = ReferenceOption.CASCADE)
     var createdAt = datetime("createdAt").defaultExpression(CurrentDateTime)
